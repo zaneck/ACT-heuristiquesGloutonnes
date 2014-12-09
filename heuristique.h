fdc3f443b31a200c6949a2ad2b91997a96ec13d2
zaneck <val.owczarek@gmail.com>
2014-12-09 15:46:18 +0100

    definition des prototypes des fonctions à implementer

---
+++ heuristique.h
@@ -0,0 +1,24 @@
+#ifndef HEURISTIQUE
+#define HEURISTIQUE
+
+
+/** 
+ * Le principe est simple; on a un sac courant; à chaque objet, on le met dans le sac
+ * courant si il y a assez de place. Sinon , on crée un nouveau sac courant et on y met l’objet;
+*/
+void nextFit(certificat_t cer);
+
+/*
+ * Pour chaque objet, on regarde si il rentre dans un des sacs créés: si oui, on le met
+ * dans le premier qui convient; sinon, on créee un nouveau sac et on y met l’objet;
+ */
+void firstFit(certificat_t cer);
+
+/*
+ * Pour chaque objet, on regarde si il rentre dans un des sacs créés: si oui, on le met
+ * dans le celui qui est le plus rempli parmi ceux qui conviennent. Sinon, on crée un nouveau sac et on y
+ * met l’objet.
+ */
+void bestFit(certificat_t cer);
+
+#endef
